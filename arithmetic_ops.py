a19bae2bc0707410b6d87b232842466325ab44ab
Vishwam Shukla <shuklavishwam21@gmail.com>
2021-05-02 18:33:22 +0530

    arithmetic and area of the traingle

---
+++ arithmetic_ops.py
@@ -0,0 +1,7 @@
+num1= int(input("Enter first number"))
+num2 = int(input("Enter second number"))
+
+print("Addition is ", num1+num2)
+print("Subtraction is ", num1-num2)
+print("Multiplication is ", num1*num2)
+print("Division is ", num1/num2)
