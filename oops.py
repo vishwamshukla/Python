44a9b50a5b9e7fa88b3f22152c7441a579687727
Vishwam Shukla <shuklavishwam@gmail.com>
2021-05-05 19:55:51 +0530

    comparison

--- oops.py
+++ oops.py
@@ -1,16 +1,39 @@
-class Computer:    #Class computer is created with method Config
+# class Computer:    #Class computer is created with method Config
     
-    def __init__(self,cpu, ram):
-        self.cpu = cpu
-        self.ram = ram    
-    def config(self):
-        print("Config for the pc is ", self.cpu, self.ram)
+#     def __init__(self,cpu, ram):
+#         self.cpu = cpu
+#         self.ram = ram    
+#     def config(self):
+#         print("Config for the pc is ", self.cpu, self.ram)
         
 
-com1 = Computer(2,4)  # object of the class computer
-com2 = Computer(5,6)
-# Computer.config(com1)
-# Computer.config(com2)
+# com1 = Computer(2,4)  # object of the class computer
+# com2 = Computer(5,6)
+# # Computer.config(com1)
+# # Computer.config(com2)
 
-com1.config()
-com2.config()
+# com1.config()
+# com2.config()
+
+
+class Student:
+    
+    def __init__(self):
+        self.name = "Vishwam"
+        self.age = 21
+    def compare(self, other):
+        if self.age == other.age:
+            return True
+        else:
+            return False
+s1 = Student()
+s1.age = 22
+s2 = Student()
+
+if s1.compare(s2):
+    print("They are same")
+else:
+    print("Not same")
+
+print(s1.name)
+print(s2.name)
