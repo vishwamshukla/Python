e9c6c131668506473fa47a8f406163d636b83b0b
Vishwam Shukla <shuklavishwam@gmail.com>
2021-05-06 00:05:01 +0530

    Constructors and Super class

--- oops.py
+++ oops.py
@@ -103,37 +103,68 @@
 # lap1.show()
 
 #inheritance
-class A:               #Parent class or Super class
+# class A:               #Parent class or Super class
+#     def feature1(self):
+#         print("Feature 1 is working")
+#     def feature2(self):
+#         print("Feature 2 is working")
+#  #Single level        
+# class B(A):            #Child class or Sub class
+#     def feature3(self):
+#         print("Feature 3 is working")
+#     def feature4(self):
+#         print("Feature 4 is working")
+
+# #Multilevel Inheritance        
+# class C(B):            #Child class or Sub class
+#     def feature5(self):
+#         print("Feature 5 is working")
+#     def feature6(self):
+#         print("Feature 6 is working")
+ 
+#  #Multiple Inheritance       
+# class C(A,B):            #Child class or Sub class
+#     def feature5(self):
+#         print("Feature 5 is working")
+#     def feature6(self):
+#         print("Feature 6 is working")
+# a1 = A()
+# a1.feature1()
+# a1.feature2()
+
+# b1=B()
+# b1.feature2()
+
+# c1=C()
+# c1.feature1()
+
+
+
+class A:    
+    #Parent class or Super class
+    
+    def __init__(self):   #Constructors
+        print("In A init")
+    
     def feature1(self):
-        print("Feature 1 is working")
+        print("Feature 1-A is working")
     def feature2(self):
         print("Feature 2 is working")
  #Single level        
-class B(A):            #Child class or Sub class
+class B:            #Child class or Sub class
+    
+    def __init__(self):
+        print("In B init")
     def feature3(self):
         print("Feature 3 is working")
     def feature4(self):
         print("Feature 4 is working")
-
-#Multilevel Inheritance        
-class C(B):            #Child class or Sub class
-    def feature5(self):
-        print("Feature 5 is working")
-    def feature6(self):
-        print("Feature 6 is working")
- 
- #Multiple Inheritance       
-class C(A,B):            #Child class or Sub class
-    def feature5(self):
-        print("Feature 5 is working")
-    def feature6(self):
-        print("Feature 6 is working")
-a1 = A()
-a1.feature1()
-a1.feature2()
-
-b1=B()
-b1.feature2()
-
-c1=C()
-c1.feature1()
+#MRO(Metod resolution order)  
+class C(A,B):
+    def __init__(self):
+        super().__init__()  #will call init of A class first(goes from left to right C->A->B)
+        print("In C init")
+    def feat(self):
+        super().feature2()
+a1 = C()
+a1.feat()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,6 +1,2 @@
-#Multilevel Inheritance        
-# class C(B):            #Child class or Sub class
-#     def feature5(self):
-#         print("Feature 5 is working")
-#     def feature6(self):
-#         print("Feature 6 is working")
+def __init__(self):
+    #     print("In B init")
