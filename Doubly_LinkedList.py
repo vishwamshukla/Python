5b7a82a8c62293ff72457b97f16a50d0e99979e5
Vishwam Shukla <shuklavishwam@gmail.com>
2021-05-31 22:55:33 +0530

    new concepts like graphs, trees and sorting added to the repo

---
+++ Doubly_LinkedList.py
@@ -0,0 +1,9 @@
+class Node:
+    def __init_(self,data):
+        self.data = data
+        self.prev = None
+        self.next = None
+class DoublyLinekdlist:
+    def __init__(self):
+        self.first = None
+        self.last = None
